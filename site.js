ea4e1f05a7de7aa4a01a981c3d41970205095fe3
M Fakhri Aldin <mfakhri.aldin@gmai.com>
2021-10-12 20:35:28 +0700

    menambakan fitur responsive dan memperbaiki tampilan web sebelumnya

--- site.js
+++ site.js
@@ -1,1 +1,8 @@
 // kode javascript untuk index.html
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+const triangle = document.querySelector(".triangle");
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  triangle.classList.toggle("active");
+});
