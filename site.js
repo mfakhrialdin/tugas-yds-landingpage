eab3a9a326d233e51f34c314374a29b521b26e86
M Fakhri Aldin <mfakhri.aldin@gmai.com>
2021-10-15 20:40:35 +0700

    membetulkan sedikit tampilan pada responsive dan menambahkan button back to top

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-// kode javascript untuk index.html
+// kode javascript untuk responsive navbar
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 const triangle = document.querySelector(".triangle");
@@ -6,3 +6,27 @@ menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   triangle.classList.toggle("active");
 });
+
+// membuat back to top button
+const btnToTop = document.getElementById("btnToTop");
+// kemunculan button saat posisi 25% dari dasar web
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1024 ||
+    document.documentElement.scrollTop > 1024
+  ) {
+    btnToTop.style.display = "block";
+  } else {
+    btnToTop.style.display = "none";
+  }
+}
+
+// saat user meng-click button, scroll kembali ke atas
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
